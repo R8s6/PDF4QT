6c0c16ad3ed31581daa04a5854f86cb97cf638e8
R8s6 <44877866+R8s6@users.noreply.github.com>
2026-09-10 18:46:53 -0700

    Preserve original text state during preview refresh

--- Pdf4QtEditorPlugins/EditorPlugin/editorplugin.cpp
+++ Pdf4QtEditorPlugins/EditorPlugin/editorplugin.cpp
@@ -937,7 +937,6 @@ bool EditorPlugin::updateTextElement(pdf::PDFPageContentElementEdited* element)
             {
                 return false;
             }
-            targetTextElement->setState(sourceElementText->getState());
             targetTextElement->setTextPath(sourceElementText->getTextPath());
             targetTextElement->setItems(sourceElementText->getItems());
             targetTextElement->setTransform(sourceElementText->getTransform());
